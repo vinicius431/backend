699b11f678e9ed30749a55ec56696d48216dbafb
Vinicius Nogueira <viniciusalvessantosngueira@gmail.com>
2025-04-18 12:41:58 -0300

    Corrige nomes das variáveis de ambiente do Cloudinary

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 const storage = new CloudinaryStorage({
